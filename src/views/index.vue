f34d5a0bfbfb9409c2da09c745708e84e9255d85
zhaop33 <zhaop33@gmail.com>
2017-12-16 18:00:14 +0800

    1

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,53 +1,65 @@
-<style scoped lang="less">
-    .index{
-        width: 100%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-logo{
+        width: 100px;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+    .layout-header{
+        height: 60px;
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-copy{
         text-align: center;
-        h1{
-            height: 150px;
-            img{
-                height: 100%;
-            }
-        }
-        h2{
-            color: #666;
-            margin-bottom: 200px;
-            p{
-                margin: 0 0 50px;
-            }
-        }
-        .ivu-row-flex{
-            height: 100%;
-        }
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+    .layout-ceiling{
+        background: #464c5b;
+        padding: 10px 0;
+        overflow: hidden;
+    }
+    .layout-ceiling-main{
+        float: right;
+        margin-right: 15px;
+    }
+    .layout-ceiling-main a{
+        color: #9ba7b5;
     }
 </style>
 <template>
-    <div class="index">
-        <Row type="flex" justify="center" align="middle">
-            <Col span="24">
-                <h1>
-                    <img src="../images/logo.png">
-                </h1>
-                <h2>
-                    <p>Welcome to your iView app!</p>
-                    <Button type="ghost" @click="handleStart">Start iView</Button>
-                </h2>
-            </Col>
-        </Row>
+    <div class="layout">
+        <div class="layout-ceiling">
+            <div class="layout-ceiling-main">
+                <a href="#">Sign up</a> |
+                <a href="#">Help Center</a> |
+                <a href="#">Security</a> |
+                <a href="#">Service</a>
+            </div>
+        </div>
+        <div class="layout-header">
+            <div class="layout-logo"></div>
+        </div>
+        <div style="height: 200px"></div>
+        <div class="layout-copy">
+            2011-2016 &copy; TalkingData
+        </div>
     </div>
 </template>
 <script>
     export default {
-        methods: {
-            handleStart () {
-                this.$Modal.info({
-                    title: 'Bravo',
-                    content: 'Now, enjoy the convenience of iView.'
-                });
-            }
-        }
+
     }
 </script>
